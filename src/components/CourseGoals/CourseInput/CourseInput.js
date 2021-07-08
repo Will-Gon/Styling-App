f5e4cb9a570b62a725d7e7375685755042d7d7cf
Will-Gon <will.colocho@gmail.com>
2021-07-07 22:39:19 -0700

    add components/css

---
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = (props) => {
+    const [enteredValue, setEnteredValue] = useState('');
+
+    const goalInputChangeHandler = (e) => {
+        setEnteredValue(e.target.value);
+    };
+
+    const formSubmitHandler = (e) => {
+        e.preventDefault();
+        props.onAddGoal(enteredValue)
+    };
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className="form-control">
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler}/>
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
+};
+
+export default CourseInput;
